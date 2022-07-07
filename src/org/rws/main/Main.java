b3d0738f953e67d6e2ac9502fcdf336006a47cfd
pointer-to-bios <yyb1738487169@163.com>
2022-07-07 22:59:59 +0800

    init

---
+++ src/org/rws/main/Main.java
@@ -0,0 +1,7 @@
+package org.rws.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/rws/ui/editor/TEditor.java
@@ -0,0 +1,10 @@
+package org.rws.ui.editor;
+
+import javax.swing.*;
+
+public class TEditor extends JPanel {
+    public TEditor() {
+        super();
+        setLayout(null);
+    }
+}
